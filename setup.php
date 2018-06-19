7a47d7e67e62819e17a896019ea65b3dd009bc77
Cédric Anne <cedric.anne@gmail.com>
2018-06-19 17:01:07 +0200

    Add 4.0.1 version definition

--- setup.php
+++ setup.php
@@ -23,7 +23,7 @@
  * -------------------------------------------------------------------------
  */
 
-define("PLUGIN_MANTIS_VERSION", "4.0.0");
+define("PLUGIN_MANTIS_VERSION", "4.0.1");
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_MANTIS_MIN_GLPI", "9.2");
